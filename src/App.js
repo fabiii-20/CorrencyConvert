776859fe72f0f9c71d760dc858f83278aa6f61b4
fazna-harees <fasnahareesfasna@gmail.com>
2024-01-26 17:19:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useState } from "react";
+
+function CurrencyConvert() {
+  const conversionInput = {
+    usd: {
+      inr: 80,
+      eur: 0.9,
+      aed: 3.67,
+    },
+    inr: {
+      aed: 0.04,
+      eur: 0.01,
+      usd: 0.012,
+    },
+    eur: {
+      usd: 1.08,
+      inr: 90,
+      aed: 3.98,
+    },
+    aed: {
+      inr: 22,
+      eur: 0.25,
+      usd: 0.27,
+    },
+  };
+  const [usdMoney, setUsdMoney] = useState(1);
+  const [targetMoney, setTargetMoney] = useState("inr");
+  const [convertedMoney, setConvertedMoney] = useState(80);
+
+  const handleChange = (e) => {
+    setTargetMoney(e.target.value);
+    const convertedMoney = usdMoney * conversionInput[e.target.value];
+    setConvertedMoney(convertedMoney);
+  };
+
+  const handleUsdChange = (e) => {
+    const money = (e.target.value);
+    setUsdMoney(money);
+    console.log("Money entered:", money);
+    const conversionRate = conversionInput["usd"][targetMoney];
+    const convertedMoney = money * conversionRate;
+    setConvertedMoney(convertedMoney);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-indigo-500">
+      <h1 className="text-3xl font-semibold mb-4">Currency Converter</h1>
+      <div className="flex flex-col items-center bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%  p-4 rounded shadow-md">
+        <h2 className="text-xl font-medium mb-2">USD value</h2>
+        <input
+          type="number"
+          value={usdMoney}
+          onChange={handleUsdChange}
+          className="border border-gray-300 rounded-md px-3 py-2 mb-2"
+        />
+        <h2 className="text-xl font-medium mb-2">Target value</h2>
+        <div className=" flow-root space-x-4">
+        <input
+          type="number"
+          value={String(convertedMoney)}
+          readOnly
+          className="border border-gray-300 rounded-md px-3 py-2 mb-2"
+        />
+        <select
+          value={String(targetMoney)}
+          onChange={handleChange}
+          className="border border-gray-300 rounded-md px-3 py-2 "
         >
-          Learn React
-        </a>
-      </header>
+          {Object.keys(conversionInput["usd"]).map((money) => (
+            <option key={money} value={money}>
+              {money.toUpperCase()}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
     </div>
   );
 }
 
-export default App;
+export default CurrencyConvert;
